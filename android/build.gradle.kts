411a16eb9e76467647699f3648a66c3467e42122
vunguyenDev92 <vunguyendev92@gmail.com>
2025-08-28 21:49:31 +0700

    feat: setup firebase

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,18 @@
+plugins {
+    id("com.android.application") apply false
+    id("com.android.library") apply false
+    id("org.jetbrains.kotlin.android") apply false
+
+    // Google Services (Firebase)
+    id("com.google.gms.google-services") version "4.4.2" apply false
+
+    // Firebase Crashlytics (tùy chọn)
+    id("com.google.firebase.crashlytics") version "3.0.2" apply false
+
+    // Firebase Performance (tùy chọn)
+    id("com.google.firebase.firebase-perf") version "1.4.2" apply false
+}
+
 allprojects {
     repositories {
         google()
